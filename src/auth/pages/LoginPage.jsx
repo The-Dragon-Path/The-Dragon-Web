f0b79d8f6c59c9f20631d81a10376d76c50f5d35
JakeTeach <j.africanor@gmail.com>
2022-09-28 17:40:37 -0500

    Se agrega modificación a background de input y padding

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,13 +1,13 @@
 export const LoginPage = () => {
   return (
     <div className="container d-flex justify-content-center">
-        <div className="mt-5 rounded p-2 bg-light login">
+        <div className="mt-5 rounded p-4 bg-light login">
             <div className="w-100 d-flex justify-content-center">
                 <img className="w-75 my-3" src="https://raw.githubusercontent.com/JakeTeach/The-drago-path-logo/main/dragonpathlogo.png" alt="The Dragon Path Logo"></img>
             </div>
             <form action="" className="d-flex flex-column justify-content-center">
-              <input type="email" autocomplete="email" placeholder="tu.email@ejemplo.me" className="form-control rounded border bg-light lh-lg mb-2"/>
-              <input type="password" autocomplete="current-password" placeholder="Contraseña" className="form-control bg-light rounded border lh-lg mb-2"/>
+              <input type="email" autocomplete="email" placeholder="tu.email@ejemplo.me" className="form-control rounded border lh-lg mb-2"/>
+              <input type="password" autocomplete="current-password" placeholder="Contraseña" className="form-control rounded border lh-lg mb-2"/>
             </form>       
             <div className="flex-row form-check form-switch">
                 <input name="user[remember_me]" type="hidden" value="0" autocomplete="off"></input>
