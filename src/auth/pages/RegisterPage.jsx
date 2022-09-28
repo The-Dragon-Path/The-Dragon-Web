2eb0994e9f131726efde0bf8f34db8a2332c6690
GaviriaAmaya <judamayaga@unal.edu.co>
2022-09-28 18:04:41 -0500

    [Update] Added initial form. Pending styles for dividers and text

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,5 +1,19 @@
 export const RegisterPage = () => {
   return (
-    <h1>RegisterPage</h1>
+    <div className="container d-flex justify-content-center">
+      <div className="mt-5 rounded p-2 bg-light register">
+        <div className="d-flex justify-content-start my-3">
+          <h3>Inicio/Registro</h3>
+        </div>
+        <form action="" className="d-flex flex-column justify-content-center">
+          <input type="email" autocomplete="email" placeholder="tu.email@ejemplo.me" className="form-control rounded border lh-lg mb-2" />
+          <input type="password" autocomplete="password" placeholder="Contraseña" className="form-control rounded border lh-lg mb-2" />
+          <input type="password" autocomplete="confirm-password" placeholder="Confirmación de contraseña" className="form-control rounded border lh-lg mb-2" />
+        </form>
+        <div className="d-flex justify-content-center my-3">
+          <button type="button" class="btn btn-outline-primary">REGISTRAR</button>
+        </div>
+      </div>
+    </div>
   )
 }
