e26e0b514b44cf02ef587c12f86e5c4fd834e216
JakeTeach <j.africanor@gmail.com>
2022-09-27 22:03:54 -0500

    Agregando maquetado de login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,5 +1,30 @@
 export const LoginPage = () => {
   return (
-    <h1>LoginPage</h1>
+    <div className="container d-flex justify-content-center">
+        <div className="mt-5 rounded p-2 bg-light login">
+            <div className="w-100 d-flex justify-content-center">
+                <img className="w-75 my-3" src="https://el-camino-del-dragon.herokuapp.com/assets/dragonpathlogo-e53a37be2d37220905f41b0266ce3b8fbe635394ef7a21c77c7db9a840ff1067.png" alt="The Dragon Path Logo"></img>
+            </div>
+            <form action="" className="d-flex flex-column justify-content-center">
+              <input type="email" autocomplete="email" placeholder="tu.email@ejemplo.me" className="form-control rounded border lh-lg mb-2"/>
+              <input type="password" autocomplete="current-password" placeholder="Contraseña" className="form-control rounded border lh-lg mb-2"/>
+            </form>       
+            <div className="flex-row form-check form-switch">
+                <input name="user[remember_me]" type="hidden" value="0" autocomplete="off"></input>
+                <input className="form-check-input" type="checkbox" value="1" name="user[remember_me]" id="user_remember_me"></input>
+                <label className="form-check-label text-muted" for="user_Recordar">Recordar</label>
+            </div>
+            <div className="d-flex justify-content-center my-3">
+                <input className="btn btn-outline-info lh-sm" type="submit" value="INICIAR"></input>
+            </div>
+            <small className="w-100 d-flex justify-content-center form-text">
+                <a className="text-decoration-none text-dark " href="#">¿No tienes una cuenta?</a>
+            </small>
+            <small className="w-100 d-flex justify-content-center form-text">
+                <a className="text-decoration-none text-dark" href="#">¿Olvidaste tu contraseña?</a>
+            </small>
+            <br></br>
+        </div>
+    </div> 
   )
 }
